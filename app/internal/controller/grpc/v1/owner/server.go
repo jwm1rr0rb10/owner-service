0a05585b7ec5b48b3ea481cf003d9125821e5685
ogamor69wm1rr0rb <ogamor6996@gmail.com>
2025-10-19 16:09:54 +0000

    [owner-service - 1] feat: upload struct

--- app/internal/controller/grpc/v1/owner/server.go
+++ app/internal/controller/grpc/v1/owner/server.go
@@ -1,1 +1,15 @@
 package owner
+
+import (
+	// standard libraries
+	"context"
+
+	//other libraries
+
+	// internal libraries
+	policyOwner "github.com/ogamor69wm1rr0rb/owner-service/app/internal/controller/grpc/v1/policy/owner"
+)
+
+type policy interface {
+	CreateOwner(context.Context, *policyOwner.CreateOwnerRequest) (*policyOwner.CreateOwnerResponse, error)
+}
